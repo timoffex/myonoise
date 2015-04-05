d40395b44a141b63931f94280cd039183da75786
timoffex <timoffex@gmail.com>
2015-04-05 03:01:11 -0700

    added detail to Gestures

--- common.h
+++ common.h
@@ -13,8 +13,35 @@ struct Angles {
     float roll, pitch, yaw;
 };
 
+enum GestureType {
+    Beat, Motion
+};
+
+enum Direction {
+    Forward, Backward, Left, Right, Up, Down
+};
+
 class Gesture {
+public:
+    virtual GestureType type() = 0;
+    int get_id() { return g_id; }
+protected:
+    int g_id;
+};
+
+class GestureBeat : public Gesture {
+public:
+    GestureType type() { return Beat; }
     
+    float strength;
+};
+
+class GestureMotion : public Gesture {
+public:
+    GestureType type() { return Motion; }
+    
+    Direction direction;
+    float rate;
 };
 
 #endif
